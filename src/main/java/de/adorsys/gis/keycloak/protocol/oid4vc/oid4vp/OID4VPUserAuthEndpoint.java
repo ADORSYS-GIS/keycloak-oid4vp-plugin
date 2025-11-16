73ed1481e3b8a3b16ac711b1ec464984b32d224d
Ingrid Kamga <xingridkamga@gmail.com>
2025-11-16 13:47:47 +0100

    Drop 'services' from package naming chain
    Signed-off-by: Ingrid Kamga <xingridkamga@gmail.com>

---
+++ src/main/java/de/adorsys/gis/keycloak/protocol/oid4vc/oid4vp/OID4VPUserAuthEndpoint.java
@@ -0,0 +1,279 @@
+package de.adorsys.gis.keycloak.protocol.oid4vc.oid4vp;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import jakarta.ws.rs.BadRequestException;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.FormParam;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.NotFoundException;
+import jakarta.ws.rs.OPTIONS;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.QueryParam;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import org.jboss.logging.Logger;
+import org.keycloak.OAuth2Constants;
+import org.keycloak.OAuthErrorException;
+import org.keycloak.authentication.AuthenticationProcessor;
+import org.keycloak.events.EventBuilder;
+import org.keycloak.models.AuthenticatorConfigModel;
+import org.keycloak.models.ClientModel;
+import org.keycloak.models.KeycloakSession;
+import de.adorsys.gis.keycloak.protocol.oid4vc.oid4vp.authenticator.SdJwtAuthRequirements;
+import de.adorsys.gis.keycloak.protocol.oid4vc.oid4vp.model.ResponseObject;
+import de.adorsys.gis.keycloak.protocol.oid4vc.oid4vp.model.dto.AuthorizationContext;
+import de.adorsys.gis.keycloak.protocol.oid4vc.oid4vp.service.AuthenticationSessionStore;
+import de.adorsys.gis.keycloak.protocol.oid4vc.oid4vp.service.AuthorizationRequestService;
+import de.adorsys.gis.keycloak.protocol.oid4vc.oid4vp.service.AuthorizationResponseService;
+import de.adorsys.gis.keycloak.protocol.oid4vc.oid4vp.service.CorsService;
+import org.keycloak.representations.idm.OAuth2ErrorRepresentation;
+import org.keycloak.services.resource.RealmResourceProvider;
+import org.keycloak.sessions.AuthenticationSessionModel;
+
+import java.util.Map;
+
+/**
+ * Endpoint class for user authentication over
+ * <a href="https://openid.net/specs/openid-4-verifiable-presentations-1_0-20.html">
+ * OpenID4VP
+ * </a>.
+ *
+ * @author <a href="mailto:Ingrid.Kamga@adorsys.com">Ingrid Kamga</a>
+ */
+public class OID4VPUserAuthEndpoint extends OID4VPUserAuthEndpointBase
+        implements RealmResourceProvider {
+
+    private static final Logger logger = Logger.getLogger(OID4VPUserAuthEndpoint.class);
+
+    public static final String REQUEST_JWT_PATH = "/request.jwt";
+    public static final String RESPONSE_URI_PATH = "/response";
+    public static final String AUTH_STATUS_PATH = "/status/{transactionId}";
+
+    private final AuthorizationRequestService authorizationRequestService;
+    private final AuthorizationResponseService authorizationResponseService;
+
+    public OID4VPUserAuthEndpoint(KeycloakSession session, EventBuilder event) {
+        super(session, event);
+        this.authorizationRequestService = new AuthorizationRequestService(session);
+        this.authorizationResponseService = new AuthorizationResponseService(session);
+    }
+
+    @OPTIONS
+    @Path("{any:.*}")
+    public Response preflight() {
+        return CorsService.openPreflight().add(Response.ok());
+    }
+
+    /**
+     * Generates an OpenID4VP authentication request for user authentication.
+     */
+    @GET
+    @Path("/request")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getAuthenticationRequest(@QueryParam(OAuth2Constants.CLIENT_ID) String clientId) {
+        logger.debug("Initiating user authentication over OpenID4VP...");
+
+        try {
+            checkClient(clientId);
+        } catch (IllegalArgumentException e) {
+            throw new BadRequestException(errorResponse(
+                    Response.Status.BAD_REQUEST,
+                    OAuthErrorException.INVALID_CLIENT,
+                    "Cannot proceed with provided client ID"
+            ), e);
+        }
+
+        AuthorizationContext authContext = startAuthentication(clientId);
+        AuthenticationSessionModel authSession = recoverAuthenticationSession(
+                authContext.getTransactionId()
+        );
+
+        return CorsService.forWebOrigins(authSession)
+                .add(Response.ok(authContext));
+    }
+
+    /**
+     * Dereferences request URIs into signed request objects.
+     */
+    @GET
+    @Path(REQUEST_JWT_PATH + "/{requestId}")
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response getSignedRequestObject(@PathParam("requestId") String requestId) {
+        logger.debug("Resolving request URI to signed request object...");
+        AuthorizationContext authorizationContext;
+
+        try {
+            var authSession = this.recoverAuthenticationSession(requestId);
+            authorizationContext = new AuthenticationSessionStore(authSession)
+                    .getAuthorizationContextByRequestId(requestId);
+        } catch (IllegalArgumentException e) {
+            throw new NotFoundException(errorResponse(
+                    Response.Status.NOT_FOUND,
+                    OAuthErrorException.INVALID_REQUEST,
+                    "Authorization context not found for request ID: " + requestId
+            ), e);
+        }
+
+        String requestObjectJwt = authorizationContext.getRequestObjectJwt();
+        return CorsService.open().add(Response.ok(requestObjectJwt));
+    }
+
+    /**
+     * Processes authentication responses from the wallet toward user authentication.
+     */
+    @POST
+    @Path(RESPONSE_URI_PATH)
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response processAuthorizationResponse(
+            @FormParam(ResponseObject.VP_TOKEN_KEY) String vpToken,
+            @FormParam(ResponseObject.PRESENTATION_SUBMISSION_KEY) String presentationSubmission,
+            @FormParam(ResponseObject.STATE_KEY) String state
+    ) {
+        logger.debug("Processing authorization response for user authentication...");
+
+        // Parse a response object from the request parameters
+        ResponseObject responseObject;
+        try {
+            responseObject = new ResponseObject(vpToken, presentationSubmission, state);
+        } catch (IllegalArgumentException | JsonProcessingException e) {
+            throw new BadRequestException(errorResponse(
+                    Response.Status.BAD_REQUEST,
+                    OAuthErrorException.INVALID_REQUEST,
+                    String.format("Unparseable response params (%s)", e.getMessage())
+            ), e);
+        }
+
+        // Recover the auth session and context given the state field
+        AuthenticationSessionModel authSession;
+        AuthorizationContext authorizationContext;
+        try {
+            authSession = this.recoverAuthenticationSession(state);
+            authorizationContext = new AuthenticationSessionStore(authSession)
+                    .getAuthorizationContextByRequestId(state);
+        } catch (IllegalArgumentException e) {
+            throw new BadRequestException(errorResponse(
+                    Response.Status.BAD_REQUEST,
+                    OAuthErrorException.INVALID_REQUEST,
+                    "Authorization context not found for state (request ID): " + state
+            ), e);
+        }
+
+        // Call delegate service to process the authorization response
+        AuthenticationProcessor authProcessor = getAuthenticationProcessor();
+        authorizationResponseService.processAuthorizationResponse(
+                responseObject, authorizationContext, authSession, authProcessor);
+
+        // Successful. Return empty object
+        return CorsService.open().add(Response.ok(Map.of()));
+    }
+
+    /**
+     * Inquire the state of an authorization request context by its transaction ID.
+     * In cross-device flows, the wallet should poll this endpoint.
+     */
+    @GET
+    @Path(AUTH_STATUS_PATH)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getAuthorizationContextState(@PathParam("transactionId") String transactionId) {
+        logger.debug("Inquiring authorization context state by transaction ID...");
+        AuthenticationSessionModel authSession;
+        AuthorizationContext authorizationContext;
+
+        try {
+            authSession = this.recoverAuthenticationSession(transactionId);
+            authorizationContext = new AuthenticationSessionStore(authSession)
+                    .getAuthorizationContextByTransactionId(transactionId);
+        } catch (IllegalArgumentException e) {
+            throw new NotFoundException(errorResponse(
+                    Response.Status.NOT_FOUND,
+                    OAuthErrorException.INVALID_REQUEST,
+                    "Authorization context not found for transaction ID: " + transactionId
+            ), e);
+        }
+
+        AuthorizationContext reducedContext = new AuthorizationContext()
+                .setStatus(authorizationContext.getStatus())
+                .setAuthorizationCode(authorizationContext.getAuthorizationCode())
+                .setError(authorizationContext.getError())
+                .setErrorDescription(authorizationContext.getErrorDescription());
+
+        return CorsService.forWebOrigins(authSession)
+                .add(Response.ok(reducedContext));
+    }
+
+    /**
+     * Initializes OpenID4VP authentication and return authorization context
+     */
+    public AuthorizationContext startAuthentication(String clientId) {
+        logger.debug("Generating new authentication context...");
+
+        ClientModel client = checkClient(clientId);
+        AuthenticationSessionModel authSession = createAuthSession(client);
+        AuthenticatorConfigModel authConfig = getSdjwtAuthenticatorConfig();
+        SdJwtAuthRequirements authReqs = new SdJwtAuthRequirements(session.getContext(), authConfig);
+
+        // Call delegate service to create an authorization request
+        AuthorizationContext authorizationContext = authorizationRequestService
+                .createAuthorizationRequest(authSession, authReqs);
+
+        return new AuthorizationContext()
+                .setAuthorizationRequest(authorizationContext.getAuthorizationRequest())
+                .setTransactionId(authorizationContext.getTransactionId());
+    }
+
+    /**
+     * Loads client model associated with the given client ID
+     */
+    public ClientModel checkClient(String clientId) {
+        ClientModel client = realm.getClientByClientId(clientId);
+
+        if (client == null) {
+            throw new IllegalArgumentException("Client is unknown");
+        }
+
+        if (!client.isEnabled()) {
+            throw new IllegalArgumentException("Client is disabled");
+        }
+
+        return client;
+    }
+
+    /**
+     * Recovers the authentication session linked to a possibly extended ID.
+     */
+    private AuthenticationSessionModel recoverAuthenticationSession(String extAuthSessionId) {
+        String authSessionId = pruneAuthSessionId(extAuthSessionId);
+        AuthenticationSessionModel authSession = getAuthSession(authSessionId)
+                .orElseThrow(() -> new IllegalArgumentException(
+                        "No authentication session attached to session ID: " + extAuthSessionId
+                ));
+
+        session.getContext().setAuthenticationSession(authSession);
+        return authSession;
+    }
+
+    /**
+     * Prepares an invalid request response with the given status and error description.
+     */
+    private Response errorResponse(Response.Status status, String error, String errorDescription) {
+        var errorResponse = new OAuth2ErrorRepresentation(error, errorDescription);
+        return CorsService.open().add(Response
+                .status(status)
+                .entity(errorResponse)
+                .type(MediaType.APPLICATION_JSON)
+        );
+    }
+
+    @Override
+    public Object getResource() {
+        return this;
+    }
+
+    @Override
+    public void close() {
+    }
+}
---
+++ src/test/java/de/adorsys/gis/keycloak/protocol/oid4vc/oid4vp/utils/SdJwtVPTestUtils.java
@@ -0,0 +1,210 @@
+package de.adorsys.gis.keycloak.protocol.oid4vc.oid4vp.utils;
+
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import dasniko.testcontainers.keycloak.KeycloakContainer;
+import de.adorsys.gis.keycloak.protocol.oid4vc.oid4vp.authenticator.SdJwtAuthenticatorFactory;
+import de.adorsys.gis.keycloak.protocol.oid4vc.oid4vp.model.ClientIdScheme;
+import de.adorsys.gis.keycloak.protocol.oid4vc.tokenstatus.ReferencedTokenValidator;
+import org.keycloak.OAuth2Constants;
+import org.keycloak.common.util.KeycloakUriBuilder;
+import org.keycloak.common.util.Time;
+import org.keycloak.crypto.AsymmetricSignatureSignerContext;
+import org.keycloak.crypto.ECDSASignatureSignerContext;
+import org.keycloak.crypto.KeyWrapper;
+import org.keycloak.crypto.SignatureSignerContext;
+import org.keycloak.jose.jwk.JWK;
+import org.keycloak.representations.IDToken;
+import org.keycloak.representations.JsonWebToken;
+import org.keycloak.sdjwt.DisclosureSpec;
+import org.keycloak.sdjwt.SdJwt;
+import org.keycloak.sdjwt.vp.KeyBindingJWT;
+import org.keycloak.sdjwt.vp.SdJwtVP;
+import org.keycloak.util.JsonSerialization;
+import org.keycloak.utils.StringUtil;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+import java.util.Map;
+import java.util.Objects;
+
+import static de.adorsys.gis.keycloak.protocol.oid4vc.BaseKeycloakTest.TEST_REALM_NAME;
+import static de.adorsys.gis.keycloak.protocol.oid4vc.tokenstatus.ReferencedTokenValidator.STATUS_FIELD;
+import static de.adorsys.gis.keycloak.protocol.oid4vc.tokenstatus.ReferencedTokenValidator.STATUS_LIST_FIELD;
+
+/**
+ * Test helper for crafting SD-JWT verifiable presentations.
+ *
+ * @author <a href="mailto:Ingrid.Kamga@adorsys.com">Ingrid Kamga</a>
+ */
+public class SdJwtVPTestUtils {
+
+    public static final int ISSUER_SIGNED_JWT_LIFESPAN_SECS = 300;
+    public static final int KB_JWT_LIFESPAN_SECS = 60;
+
+    public static final String EXP_CLAIM_KEY = "exp";
+    public static final String CNF_CLAIM_KEY = "cnf";
+    public static final String JWK_CLAIM_KEY = "jwk";
+
+    private final KeycloakContainer keycloak;
+
+    public SdJwtVPTestUtils(KeycloakContainer keycloak) {
+        this.keycloak = keycloak;
+    }
+
+    /**
+     * Requests that Keycloak issue an SD-JWT credential.
+     */
+    public String requestSdJwtCredential(String vct, String username) {
+        return requestSdJwtCredential(vct, username, true, true);
+    }
+
+    /**
+     * Requests that Keycloak issue an SD-JWT credential.
+     *
+     * @param vct            The verifiable credential type
+     * @param username       The username of the user whom the credential is issued for
+     * @param setKid         Specifies if the ID of the key used by Keycloak for issuing the credential
+     *                       should be set to the `kid` header of the SD-JWT
+     * @param setStatusClaim Specifies whether to include a status claim in the issued credential
+     */
+    public String requestSdJwtCredential(String vct, String username, boolean setKid, boolean setStatusClaim) {
+
+        SignatureSignerContext signer;
+
+        try {
+            KeyWrapper keyWrapper = RSATestUtils.getRsaKeyWrapper(getKeycloakJwk());
+            if (!setKid) {
+                keyWrapper.setKid(null);
+            }
+
+            signer = new AsymmetricSignatureSignerContext(keyWrapper);
+        } catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
+            throw new RuntimeException(e);
+        }
+
+        String serverUrl = keycloak.getAuthServerUrl();
+        String keycloakIssuerURI = KeycloakUriBuilder.fromUri(serverUrl)
+                .path("/realms/{realm}")
+                .build(TEST_REALM_NAME)
+                .toString();
+
+        SdJwt sdJwt = exampleSdJwtCredential(keycloakIssuerURI, vct, username, setStatusClaim)
+                .withSigner(signer)
+                .build();
+
+        return sdJwt.toSdJwtString();
+    }
+
+    /**
+     * Scaffold an SD-JWT identity credential that can clear authentication.
+     */
+    private static SdJwt.Builder exampleSdJwtCredential(
+            String iss, String vct, String username, boolean setStatusClaim
+    ) {
+        Objects.requireNonNull(iss);
+        Objects.requireNonNull(vct);
+
+        ObjectNode claimSet = JsonSerialization.mapper.createObjectNode();
+        claimSet.put(OAuth2Constants.ISSUER, iss);
+        claimSet.put(SdJwtAuthenticatorFactory.VCT_CONFIG, vct);
+        claimSet.put(EXP_CLAIM_KEY, Time.currentTime() + ISSUER_SIGNED_JWT_LIFESPAN_SECS);
+
+        // Add status list claim (Token Status List)
+        if (setStatusClaim) {
+            claimSet.set(STATUS_FIELD, JsonSerialization.mapper.valueToTree(
+                    Map.of(STATUS_LIST_FIELD, new ReferencedTokenValidator.StatusInfo(
+                            0, "https://example.com/status-list-jwt"
+                    ))
+            ));
+        }
+
+        DisclosureSpec.Builder disclosure = DisclosureSpec.builder()
+                .withDecoyClaim("G02NSrQfjFXQ7Io09syajA");
+
+        // Bind credential to user
+        JWK jwk = ECTestUtils.getECPublicJwk(getUserJwk());
+        ObjectNode cnf = JsonSerialization.mapper.createObjectNode();
+        cnf.set(JWK_CLAIM_KEY, JsonSerialization.mapper.valueToTree(jwk));
+        claimSet.set(CNF_CLAIM_KEY, cnf);
+
+        if (username != null) {
+            claimSet.put(OAuth2Constants.USERNAME, username);
+            disclosure = disclosure.withUndisclosedClaim(OAuth2Constants.USERNAME, "eI8ZWm9QnKPpNPeNenHdhQ");
+        }
+
+        return SdJwt.builder()
+                .withDisclosureSpec(disclosure.build())
+                .withClaimSet(claimSet);
+    }
+
+    /**
+     * Creates an SD-JWT verifiable presentation of an SD-JWT credential.
+     */
+    public String presentSdJwt(String sdjwt, String nonce, String aud, JWK holderKey)
+            throws Exception {
+        return presentSdJwt(sdjwt, nonce, aud, holderKey, KB_JWT_LIFESPAN_SECS);
+    }
+
+    /**
+     * Creates an SD-JWT verifiable presentation of an SD-JWT credential.
+     *
+     * @param sdjwt         The SD-JWT credential (without key-binding JWT)
+     * @param nonce         A nonce value for replay protection
+     * @param aud           An audience for replay protection
+     * @param holderKey     The holder's private key
+     * @param kbJwtLifespan The validity of the key-binding JWT in seconds
+     */
+    public String presentSdJwt(String sdjwt, String nonce, String aud, JWK holderKey, long kbJwtLifespan)
+            throws Exception {
+        JsonWebToken kbJwtClaims = new JsonWebToken();
+
+        long currentTime = Time.currentTime();
+        kbJwtClaims.iat(currentTime);
+        kbJwtClaims.exp(currentTime + kbJwtLifespan);
+
+        // TODO: Remove the explicit scheme below when Keycloak supports aud pattern matching
+        String clientIdScheme = ClientIdScheme.X509_SAN_DNS.getValue().toLowerCase();
+        if (StringUtil.isNotBlank(aud) && !aud.startsWith(clientIdScheme + ":")) {
+            aud = String.format("%s:%s", clientIdScheme, aud);
+        }
+
+        kbJwtClaims.getOtherClaims().put(IDToken.NONCE, nonce);
+        kbJwtClaims.getOtherClaims().put(IDToken.AUD, aud);
+
+        KeyWrapper keyWrapper = ECTestUtils.getEcKeyWrapper(holderKey);
+        SignatureSignerContext signer = new ECDSASignatureSignerContext(keyWrapper);
+
+        SdJwtVP sdJwtVP = SdJwtVP.of(sdjwt);
+        return sdJwtVP.present(
+                null,
+                JsonSerialization.mapper.valueToTree(kbJwtClaims),
+                signer,
+                KeyBindingJWT.TYP
+        );
+    }
+
+    public static JWK getKeycloakJwk() {
+        return testJwkResource("/keys/keycloak.json");
+    }
+
+    public static JWK getUserJwk() {
+        return testJwkResource("/keys/user-wallet-key.json");
+    }
+
+    public static JWK getStrayJwk() {
+        return testJwkResource("/keys/stray-key.json");
+    }
+
+    /**
+     * Load a test resource file, assuming it is a JWK.
+     */
+    private static JWK testJwkResource(String filename) {
+        try (InputStream stream = SdJwtVPTestUtils.class.getResourceAsStream(filename)) {
+            return JsonSerialization.readValue(stream, JWK.class);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
