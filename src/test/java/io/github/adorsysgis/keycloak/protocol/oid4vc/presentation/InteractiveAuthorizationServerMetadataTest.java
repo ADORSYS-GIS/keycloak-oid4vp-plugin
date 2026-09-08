fa7cd11cc3a187a1aed2c8dd83eada9c8aff39b6
Ogenbertrand <ogenbertrand@gmail.com>
2026-09-08 14:10:16 +0100

    Test delegated wallet attestation validation

--- src/test/java/io/github/adorsysgis/keycloak/protocol/oid4vc/presentation/InteractiveAuthorizationServerMetadataTest.java
+++ src/test/java/io/github/adorsysgis/keycloak/protocol/oid4vc/presentation/InteractiveAuthorizationServerMetadataTest.java
@@ -6,6 +6,7 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import com.fasterxml.jackson.databind.JsonNode;
 import io.github.adorsysgis.keycloak.protocol.oid4vc.oid4vp.OID4VPBaseKeycloakTest;
 import io.github.adorsysgis.keycloak.protocol.oid4vc.patch.metadata.OID4VCIssuerMetadataProvider;
+import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.List;
@@ -13,7 +14,9 @@ import java.util.Map;
 import java.util.Optional;
 import org.apache.http.HttpResponse;
 import org.apache.http.HttpStatus;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
 import org.junit.jupiter.api.DisplayName;
@@ -21,7 +24,9 @@ import org.junit.jupiter.api.Test;
 import org.keycloak.OAuth2Constants;
 import org.keycloak.OAuthErrorException;
 import org.keycloak.admin.client.resource.RealmResource;
+import org.keycloak.authentication.authenticators.client.AttestationBasedClientAuthenticator;
 import org.keycloak.protocol.oidc.utils.PkceUtils;
+import org.keycloak.representations.idm.ClientRepresentation;
 import org.keycloak.representations.idm.OAuth2ErrorRepresentation;
 import org.keycloak.representations.idm.RealmRepresentation;
 import org.keycloak.util.JsonSerialization;
@@ -116,6 +121,54 @@ class InteractiveAuthorizationServerMetadataTest extends OID4VPBaseKeycloakTest 
         }
     }
 
+    @Test
+    @DisplayName("wallet attestation validation is delegated to Keycloak's client authenticator")
+    void walletAttestationValidationUsesKeycloakAuthenticator() throws Exception {
+        RealmResource realm = keycloak.getKeycloakAdminClient().realm(getActiveTestRealm());
+        var clientResource = realm.clients().findByClientId(TEST_CLIENT_ID).stream()
+                .findFirst()
+                .map(client -> realm.clients().get(client.getId()))
+                .orElseThrow();
+        ClientRepresentation originalClient = clientResource.toRepresentation();
+        RealmRepresentation realmRepresentation = realm.toRepresentation();
+        Map<String, String> attributes = new HashMap<>(
+                Optional.ofNullable(realmRepresentation.getAttributes()).orElseGet(Map::of));
+        String originalRequirement = attributes.get(AuthorizationChallengeEndpoint.ATTR_REQUIRE_WALLET_ATTESTATION);
+
+        try {
+            ClientRepresentation configuredClient = clientResource.toRepresentation();
+            configuredClient.setClientAuthenticatorType(AttestationBasedClientAuthenticator.PROVIDER_ID);
+            clientResource.update(configuredClient);
+
+            attributes.put(AuthorizationChallengeEndpoint.ATTR_REQUIRE_WALLET_ATTESTATION, "true");
+            realmRepresentation.setAttributes(attributes);
+            realm.update(realmRepresentation);
+
+            var codeVerifier = PkceUtils.generateCodeVerifier();
+            var codeChallenge = PkceUtils.encodeCodeChallenge(codeVerifier, OAuth2Constants.PKCE_METHOD_S256);
+            HttpResponse response = postAuthorizationChallengeWithAttestation(List.of(
+                    new BasicNameValuePair(OAuth2Constants.CLIENT_ID, TEST_CLIENT_ID),
+                    new BasicNameValuePair(OAuth2Constants.SCOPE, OAuth2Constants.SCOPE_OPENID),
+                    new BasicNameValuePair(
+                            AuthorizationChallengeEndpoint.INTERACTION_TYPES_SUPPORTED_PARAM,
+                            AuthorizationChallengeEndpoint.INTERACTION_OPENID4VP_PRESENTATION),
+                    new BasicNameValuePair(OAuth2Constants.CODE_CHALLENGE, codeChallenge),
+                    new BasicNameValuePair(OAuth2Constants.CODE_CHALLENGE_METHOD, OAuth2Constants.PKCE_METHOD_S256)));
+
+            assertEquals(HttpStatus.SC_UNAUTHORIZED, response.getStatusLine().getStatusCode());
+            OAuth2ErrorRepresentation error = parseHttpResponse(response, OAuth2ErrorRepresentation.class);
+            assertEquals(OAuthErrorException.INVALID_CLIENT, error.getError());
+        } finally {
+            clientResource.update(originalClient);
+            restoreAttribute(
+                    realm,
+                    realmRepresentation,
+                    attributes,
+                    AuthorizationChallengeEndpoint.ATTR_REQUIRE_WALLET_ATTESTATION,
+                    originalRequirement);
+        }
+    }
+
     private void updateAttribute(
             RealmResource realm, RealmRepresentation rep, Map<String, String> attributes, String value) {
         attributes.put(OID4VCIssuerMetadataProvider.ATTR_PRESENTATION_DURING_ISSUANCE, value);
@@ -140,5 +193,18 @@ class InteractiveAuthorizationServerMetadataTest extends OID4VPBaseKeycloakTest 
         assertEquals(HttpStatus.SC_OK, response.getStatusLine().getStatusCode());
         String payload = EntityUtils.toString(response.getEntity(), StandardCharsets.UTF_8);
         return JsonSerialization.mapper.readTree(payload);
+    }
+
+    private HttpResponse postAuthorizationChallengeWithAttestation(List<BasicNameValuePair> form) throws IOException {
+        String url = getTestRealmEndpoint() + "/" + AuthorizationChallengeEndpointFactory.PROVIDER_ID;
+        HttpPost post = new HttpPost(url);
+        post.setEntity(new UrlEncodedFormEntity(form));
+        post.setHeader(
+                AttestationBasedClientAuthenticator.OAUTH_CLIENT_ATTESTATION_HEADER,
+                "eyJ0eXAiOiJub3QtYXR0ZXN0YXRpb24ifQ.eA.eA");
+        post.setHeader(
+                AttestationBasedClientAuthenticator.OAUTH_CLIENT_ATTESTATION_POP_HEADER,
+                "eyJ0eXAiOiJub3QtYXR0ZXN0YXRpb24ifQ.eA.eA");
+        return httpClient.execute(post);
     }
 }
